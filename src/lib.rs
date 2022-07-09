7cf397f82803774ed1179b2e78f8bb46e4ecd5f1
Dominic van Berkel <dominic@baudvine.net>
2022-07-09 19:10:42 +0200

    we can download a schedule

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,38 @@
+use std::{fs::File, io::Write};
+
+use curl::easy::Easy;
+use tempfile;
+
+fn fetch_to(mut file: File) -> Result<(), curl::Error> {
+    let mut easy = Easy::new();
+    easy.url("https://program.mch2022.org/mch2021-2020/schedule/export/schedule.json")?;
+    easy.write_function(move |data| {
+        // unpacking this to match write_function's error handling just obscures
+        // write_all's error, so never mind that.
+        file.write_all(data).unwrap();
+        Ok(data.len())
+    })?;
+    easy.perform()
+}
+
+fn fetch() -> File {
+    let file = tempfile::tempfile().unwrap();
+    fetch_to(file.try_clone().unwrap()).unwrap();
+    return file;
+}
+
 pub fn convert() {
-    
+    let schedule_json = fetch();
 }
 
 #[cfg(test)]
 mod tests {
+    use std::{fs::File, os::linux::fs::MetadataExt};
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn fetch_to() {
+        let file = File::create("fetch_to.json").unwrap();
+        super::fetch_to(file.try_clone().unwrap()).unwrap();
+        assert!(file.metadata().unwrap().st_size() > 1024);
+        std::fs::remove_file("fetch_to.json").unwrap();
     }
 }
