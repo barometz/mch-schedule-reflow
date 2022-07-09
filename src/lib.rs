cf87d1492a02e514bdd84454848d65ed3a105894
Dominic van Berkel <dominic@baudvine.net>
2022-07-09 21:26:08 +0200

    parse json

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,40 @@
-use std::{fs::File, io::Write};
+use std::{
+    fs::File,
+    io::{Read, Seek, Write},
+};
 
 use curl::easy::Easy;
+use json;
 use tempfile;
 
-fn fetch_json_to(mut file: File) -> Result<(), curl::Error> {
+fn fetch_json_to(mut output: File) -> Result<(), curl::Error> {
     let mut request = Easy::new();
     request.url("https://program.mch2022.org/mch2021-2020/schedule/export/schedule.json")?;
     request.write_function(move |data| {
         // unpacking this to match write_function's error handling just obscures
         // write_all's error, so never mind that.
-        file.write_all(data).unwrap();
+        output.write_all(data).unwrap();
         Ok(data.len())
     })?;
     request.perform()
 }
 
 fn fetch_json() -> File {
-    let file = tempfile::tempfile().unwrap();
+    let mut file = tempfile::tempfile().unwrap();
     fetch_json_to(file.try_clone().unwrap()).unwrap();
+    file.rewind().unwrap();
     return file;
 }
 
 pub fn convert() {
-    let schedule_json = fetch_json();
+    let schedule_json = {
+        let mut schedule_txt = String::new();
+        fetch_json()
+            .read_to_string(&mut schedule_txt)
+            .map(|_| json::parse(&schedule_txt))
+            .unwrap()
+            .unwrap()
+    };
 }
 
 #[cfg(test)]
@@ -34,5 +46,10 @@ mod tests {
         super::fetch_json_to(file.try_clone().unwrap()).unwrap();
         assert!(file.metadata().unwrap().st_size() > 1024);
         std::fs::remove_file("fetch_to.json").unwrap();
+    }
+
+    #[test]
+    fn convert() {
+        super::convert();
     }
 }
